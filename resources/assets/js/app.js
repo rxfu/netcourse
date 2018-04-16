5985dc3b50cf2cabc4cccabf10ed811ae4027a09
stephen <stephen.frx@gmail.com>
2018-04-16 22:12:21 +0800

    add vue components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,14 +9,26 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import VueRouter from 'vue-router';
+
+import routes from './routes.js';
+import App from './pages/App.vue';
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+
+const router = new VueRouter({
+	mode: 'history',
+	routes
+})
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    render: h => h(App),
+    router
 });
